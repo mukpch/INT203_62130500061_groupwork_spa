f19705b6e9942254c60d1f3a06a184fa1cb7a8d9
mukpch <pacharaporn.ksn@mail.kmutt.ac.th>
2021-04-18 18:04:21 +0700

    [62130500061] add files in views

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="w-full">
+    <div class="w-full flex justify-center">
+      <h1 class="text-xl font-semibold text-purple-900 mt-28">About Us</h1>
+    </div>
+  </div>
+
+  <div class="text-md w-full flex items-center justfiy-center my-10">
+    <div class="w-2/6 shadow-xl ml-56">
+      <img :src="img" class="rounded-md" />
+    </div>
+
+    <div class="pl-20">
+      <base-card class="px-20 py-20 text-lg text-purple-800">
+        <p>Name : {{ name }}</p>
+        <p>Student id : {{ id }}</p>
+        <p>Nickname : {{ nickname }}</p>
+        <p>Instagram : {{ instagram }}</p>
+        <p>Facebook : {{ facebook }}</p>
+        <p>Email : {{ email }}</p>
+        <p>Phone : {{ phone }}</p>
+      </base-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Pacharaporn Kamsanong",
+      nickname: "Muk",
+      id: "62130500061",
+      instagram: "mukpch",
+      facebook: "Pacharaporn Kamsanong",
+      email: "pacharaporn.ksn@gmail.com",
+      phone: "080-776-4826",
+      img: require("../assets/aboutus.jpg"),
+    };
+  },
+};
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full">
+
+    <h1 class=" text-purple-500 text-md flex justify-end pt-20 mr-10">waste your time and waste of your life ,</h1>
+
+    <h1 class=" text-yellow-500 text-md flex justify-end pt-2 mr-10">OR master your time and master your life.</h1>
+
+    <h1 class="font-semibold text-red-400 text-4xl flex justify-center pt-36">Hey guys!</h1>
+
+    <h1 class="font-semibold text-red-400 text-2xl flex justify-center pt-5">Are u raedy to do the TIMETABLE ?</h1>
+
+    <div class="pt-20 text-purple-500">
+      <hover-click>
+        <router-link to="/timetable" class="flex justify-center font-semibold text-xl">Yep!</router-link>
+      </hover-click>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import HoverClick from '../components/HoverClick.vue';
+export default {
+  components: { HoverClick },
+};
+</script>
