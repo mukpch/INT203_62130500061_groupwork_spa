0f79b9834dec469ccc4631898bd9e01c5505cb11
mukpch <pacharaporn.ksn@mail.kmutt.ac.th>
2021-04-19 01:06:06 +0700

    [62130500061] add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
+import aboutus from '../views/About.vue'
+
+const routes = [{
+    component: Home,
+    path: "/",
+    name: "home"
+},
+{
+    component: aboutus,
+    path: "/aboutus",
+    name: "aboutus",
+},
+
+]
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
