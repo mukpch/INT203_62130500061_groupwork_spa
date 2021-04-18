9deef8d5e04fa712a613ce47a2b705e9e8a46ac0
mukpch <pacharaporn.ksn@mail.kmutt.ac.th>
2021-04-18 18:24:51 +0700

    [62130500061] add NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    <hover-click>
+      <router-link to="/" class="px-8">Home</router-link>
+    </hover-click>
+    <hover-click>
+      <router-link to="/timetable" class="px-8">Timetable</router-link>
+    </hover-click>
+    <hover-click>
+      <router-link to="/aboutus" class="px-8">About Us</router-link>
+    </hover-click>
+  </div>
+</template>
+
+<script>
+import HoverClick from './HoverClick.vue';
+export default {
+  components: { HoverClick },
+};
+</script>
